a9aa66eef541507b14ed1612491d3aada147d0dd
Marek Zelinka <mzelinka17@gmail.com>
2025-03-14 09:55:17 +0100

    add primary font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
+import { inter } from "@/app/ui/fonts";
 import "@/app/ui/global.css";
+import clsx from "clsx";
 
 export default function RootLayout({
   children,
@@ -7,7 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={clsx("antialiased", inter.className)}>{children}</body>
     </html>
   );
 }
