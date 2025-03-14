81691e8ca7961d044a743bfed239f0c8521ebb86
Marek Zelinka <mzelinka17@gmail.com>
2025-03-14 11:51:04 +0100

    fix adming routes

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -102,16 +102,19 @@ async function seedRevenue() {
 }
 
 export async function GET() {
-  try {
-    const result = await sql.begin((sql) => [
-      seedUsers(),
-      seedCustomers(),
-      seedInvoices(),
-      seedRevenue(),
-    ]);
+  return Response.json({
+    message: "Uncomment the lines below and remove this line. ",
+  });
+  // try {
+  //   const result = await sql.begin((sql) => [
+  //     seedUsers(),
+  //     seedCustomers(),
+  //     seedInvoices(),
+  //     seedRevenue(),
+  //   ]);
 
-    return Response.json({ message: "Database seeded successfully" });
-  } catch (error) {
-    return Response.json({ error }, { status: 500 });
-  }
+  //   return Response.json({ message: "Database seeded successfully" });
+  // } catch (error) {
+  //   return Response.json({ error }, { status: 500 });
+  // }
 }
