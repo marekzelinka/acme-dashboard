0547b11797e0e9eee8d23563f574da876697e892
Marek Zelinka <mzelinka17@gmail.com>
2025-03-14 10:11:16 +0100

    add hero images

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { lusitana } from "@/app/ui/fonts";
 import styles from "@/app/ui/home.module.css";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import clsx from "clsx";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Page() {
@@ -34,7 +35,20 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            alt=""
+            width={1000}
+            height={760}
+            className="hidden md:block"
+          />
+          <Image
+            src="/hero-mobile.png"
+            alt=""
+            width={560}
+            height={620}
+            className="md:hidden"
+          />
         </div>
       </div>
     </main>
