87d25edca0229e017492d0ce8b305ca1ccc51f59
Marek Zelinka <mzelinka17@gmail.com>
2025-03-14 09:59:42 +0100

    add image

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,10 +1,14 @@
 import { lusitana } from "@/app/ui/fonts";
 import { GlobeAltIcon } from "@heroicons/react/24/outline";
+import clsx from "clsx";
 
 export default function AcmeLogo() {
   return (
     <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+      className={clsx(
+        lusitana.className,
+        "flex flex-row items-center leading-none text-white",
+      )}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
       <p className="text-[44px]">Acme</p>
