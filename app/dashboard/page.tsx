9ad9528ad4d6f4f41d99994984fe20bc71bfadac
Marek Zelinka <mzelinka17@gmail.com>
2025-03-14 12:55:46 +0100

    fetch data for revenue chart

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,20 @@
-export default function Page() {
-  return <p>Dashboard page</p>;
+import { fetchRevenue } from "@/app/lib/data";
+import { lusitana } from "@/app/ui/fonts";
+import clsx from "clsx";
+import RevenueChart from "../ui/dashboard/revenue-chart";
+
+export default async function Page() {
+  const revenue = await fetchRevenue();
+
+  return (
+    <main>
+      <h1 className={clsx(lusitana.className, "mb-4 text-xl md:text-2xl")}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4"></div>
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue} />
+      </div>
+    </main>
+  );
 }
